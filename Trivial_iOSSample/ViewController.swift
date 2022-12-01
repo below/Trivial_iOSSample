f69a84a2d912a0b2fd1d5accf6a8889bffe6cd6f
Alexander von Below <alex@vonbelow.com>
2022-12-01 10:27:40 +0100

    Initial Commit

---
+++ Trivial_iOSSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trivial_iOSSample
+//
+//  Created by Alexander von Below on 01.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
